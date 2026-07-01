f81ea93ef0cbd060ebb0e8fbdec4ad21fea97bdc
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-01 19:19:16 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/russianpath/MainActivity.kt
+++ app/src/main/java/com/example/russianpath/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.russianpath.presentation.screens.dashboard.DashboardScreen
+import com.example.russianpath.presentation.navigation.NavGraph
 import com.example.russianpath.presentation.theme.RussianPathTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RussianPathTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    DashboardScreen()
+                    NavGraph()
                 }
             }
         }
