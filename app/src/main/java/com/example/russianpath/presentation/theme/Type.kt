b2d9415e4434fdfd32b34588017d99e9355a8bbd
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-01 14:24:40 +0300

    Add Typography definitions for text styles

---
+++ app/src/main/java/com/example/russianpath/presentation/theme/Type.kt
@@ -0,0 +1,15 @@
+package com.example.russianpath.presentation.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    headlineLarge = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp),
+    headlineSmall = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp),
+    titleMedium = TextStyle(fontWeight = FontWeight.Medium, fontSize = 16.sp),
+    bodyLarge = TextStyle(fontSize = 16.sp),
+    bodyMedium = TextStyle(fontSize = 14.sp),
+    bodySmall = TextStyle(fontSize = 12.sp)
+)
