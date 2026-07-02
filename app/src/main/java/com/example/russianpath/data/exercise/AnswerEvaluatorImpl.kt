40cc773c560c0cd27c915045aede7abe90ca8053
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 01:42:56 +0300

    Update AnswerEvaluatorImpl.kt

--- app/src/main/java/com/example/russianpath/data/exercise/AnswerEvaluatorImpl.kt
+++ app/src/main/java/com/example/russianpath/data/exercise/AnswerEvaluatorImpl.kt
@@ -1,7 +1,13 @@
 package com.example.russianpath.data.exercise
 
-import com.example.russianpath.core.exercise.*
-import com.example.russianpath.core.progress.*
+import com.example.russianpath.core.exercise.ChoiceAnswer
+import com.example.russianpath.core.exercise.ChoiceUserAnswer
+import com.example.russianpath.core.exercise.Exercise
+import com.example.russianpath.core.exercise.TextAnswer
+import com.example.russianpath.core.exercise.TextUserAnswer
+import com.example.russianpath.core.progress.AnswerEvaluator
+import com.example.russianpath.core.progress.AnswerResult
+import com.example.russianpath.core.progress.UserAnswer
 import java.time.Instant
 import javax.inject.Inject
 import javax.inject.Singleton
