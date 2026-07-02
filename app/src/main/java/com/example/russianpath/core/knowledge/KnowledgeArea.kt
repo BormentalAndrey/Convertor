a5924fde15f4005da768d3e63cfdcf6c58619335
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 00:26:14 +0300

    Create KnowledgeArea.kt

---
+++ app/src/main/java/com/example/russianpath/core/knowledge/KnowledgeArea.kt
@@ -0,0 +1,11 @@
+package com.example.russianpath.core.knowledge
+
+enum class KnowledgeArea {
+    GRAPHICS,
+    PHONETICS,
+    ORTHOGRAPHY,
+    MORPHEMICS,
+    MORPHOLOGY,
+    SYNTAX,
+    TEXT
+}
