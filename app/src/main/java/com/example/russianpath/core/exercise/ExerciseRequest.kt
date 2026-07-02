a638563265f4e4e6257bc9b3789fbc7abb93e0d4
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-02 23:31:18 +0300

    Create ExerciseRequest.kt

---
+++ app/src/main/java/com/example/russianpath/core/exercise/ExerciseRequest.kt
@@ -0,0 +1,11 @@
+package com.example.russianpath.core.exercise
+
+import com.example.russianpath.core.analysis.WordAnalysis
+import com.example.russianpath.core.knowledge.SkillCode
+
+data class ExerciseRequest(
+    val skillCode: SkillCode,
+    val exerciseType: ExerciseType,
+    val difficulty: Difficulty,
+    val analysis: WordAnalysis
+)
