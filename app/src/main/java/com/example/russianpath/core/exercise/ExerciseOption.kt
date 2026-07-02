9f64aeafcc84c0a1ac71035a164e86362f90bb31
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 00:56:37 +0300

    Update ExerciseOption.kt

--- app/src/main/java/com/example/russianpath/core/exercise/ExerciseOption.kt
+++ app/src/main/java/com/example/russianpath/core/exercise/ExerciseOption.kt
@@ -3,6 +3,6 @@ package com.example.russianpath.core.exercise
 sealed interface ExerciseOption
 
 data class TextOption(
-    val id: String,
+    val id: OptionId,
     val text: String
 ) : ExerciseOption
