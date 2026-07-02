f559820388842f71bb8f6bc51b8059c06352b58a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-02 23:25:42 +0300

    Create Analyses.kt

---
+++ app/src/main/java/com/example/russianpath/core/analysis/Analyses.kt
@@ -0,0 +1,10 @@
+package com.example.russianpath.core.analysis
+
+data class Analyses(
+    val syllable: SyllableAnalysis? = null
+    // v2: val sound: SoundAnalysis? = null,
+    // v2: val morpheme: MorphemeAnalysis? = null,
+    // v2: val morphology: MorphologyAnalysis? = null,
+    // v2: val orthography: OrthographyAnalysis? = null,
+    // v2: val syntax: SyntaxAnalysis? = null
+)
