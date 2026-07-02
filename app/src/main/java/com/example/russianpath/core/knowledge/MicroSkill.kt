1f32cdb47ec0103781de014e380bbe5a246cf8a5
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-02 23:33:27 +0300

    Create MicroSkill.kt

---
+++ app/src/main/java/com/example/russianpath/core/knowledge/MicroSkill.kt
@@ -0,0 +1,9 @@
+package com.example.russianpath.core.knowledge
+
+data class MicroSkill(
+    val id: String,
+    val objectiveId: String,
+    val skillCode: SkillCode,
+    val name: String,
+    val description: String
+)
