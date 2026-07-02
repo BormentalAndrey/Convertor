838052adffaee9012c3c8f80cf239b151b1cc276
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 01:19:38 +0300

    Update GradeDao.kt

--- app/src/main/java/com/example/russianpath/data/local/dao/GradeDao.kt
+++ app/src/main/java/com/example/russianpath/data/local/dao/GradeDao.kt
@@ -1,16 +1,13 @@
 package com.example.russianpath.data.local.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.example.russianpath.data.local.entity.GradeEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface GradeDao {
     @Query("SELECT * FROM grades ORDER BY sortOrder")
-    fun getAll(): Flow<List<GradeEntity>>
+    fun observeAll(): Flow<List<GradeEntity>>
 
     @Query("SELECT * FROM grades WHERE id = :id")
     suspend fun getById(id: String): GradeEntity?
