43558b203ff60cbfebb685b6043071ec1a7c3025
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 01:41:04 +0300

    Update TemplateEngine.kt

--- app/src/main/java/com/example/russianpath/core/exercise/TemplateEngine.kt
+++ app/src/main/java/com/example/russianpath/core/exercise/TemplateEngine.kt
@@ -2,7 +2,6 @@ package com.example.russianpath.core.exercise
 
 interface TemplateEngine {
     fun buildPrompt(request: ExerciseRequest): String
-    fun buildOptions(request: ExerciseRequest): List<ExerciseOption>
     fun buildHint(request: ExerciseRequest): String
     fun buildCorrectAnswer(request: ExerciseRequest): CorrectAnswer
 }
