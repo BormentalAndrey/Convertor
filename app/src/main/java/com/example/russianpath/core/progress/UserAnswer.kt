f4d1241105f73fff6b24ff5673b2f1c480b60197
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 01:47:44 +0300

    Update UserAnswer.kt

--- app/src/main/java/com/example/russianpath/core/progress/UserAnswer.kt
+++ app/src/main/java/com/example/russianpath/core/progress/UserAnswer.kt
@@ -1,6 +1,8 @@
 package com.example.russianpath.core.progress
 
+import com.example.russianpath.core.exercise.OptionId
+
 sealed interface UserAnswer
 
 data class TextUserAnswer(val text: String) : UserAnswer
-data class ChoiceUserAnswer(val index: Int) : UserAnswer
+data class ChoiceUserAnswer(val optionId: OptionId) : UserAnswer
