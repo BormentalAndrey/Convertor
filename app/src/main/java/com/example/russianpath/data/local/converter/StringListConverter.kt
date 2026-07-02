e1276585ba61f60d8d3feff08404cbbfed1b4bd5
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 01:18:35 +0300

    Create StringListConverter.kt

---
+++ app/src/main/java/com/example/russianpath/data/local/converter/StringListConverter.kt
@@ -0,0 +1,19 @@
+package com.example.russianpath.data.local.converter
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class StringListConverter {
+
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromStringList(list: List<String>): String = gson.toJson(list)
+
+    @TypeConverter
+    fun toStringList(json: String): List<String> {
+        val type = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(json, type)
+    }
+}
