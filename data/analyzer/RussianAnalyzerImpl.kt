afd1c58211cddd4bd2d8ef2319b91bdab0d58015
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-02 22:54:15 +0300

    Create RussianAnalyzerImpl.kt

---
+++ data/analyzer/RussianAnalyzerImpl.kt
@@ -0,0 +1,29 @@
+package com.example.russianpath.data.analyzer
+
+import com.example.russianpath.core.analysis.LetterAnalysis
+import com.example.russianpath.core.analysis.RussianAnalyzer
+import com.example.russianpath.core.analysis.SyllableAnalysis
+import com.example.russianpath.core.analysis.WordAnalysis
+import com.example.russianpath.core.dictionary.DictionaryWord
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class RussianAnalyzerImpl @Inject constructor(
+    private val letterAnalyzer: LetterAnalyzer,
+    private val syllableAnalyzer: SyllableAnalyzer
+) : RussianAnalyzer {
+
+    override fun analyze(word: DictionaryWord): WordAnalysis {
+        val letterAnalysis = letterAnalyzer.analyze(word.word)
+        val syllableAnalysis = syllableAnalyzer.analyze(word)
+
+        return WordAnalysis(
+            dictionaryWord = word,
+            letterAnalysis = letterAnalysis,
+            analyses = analyses {
+                syllable = syllableAnalysis
+            }
+        )
+    }
+}
