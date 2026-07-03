ea8af71a071bc3567785a2ea06a91a4ef80c6cba
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 04:06:14 +0300

    Update SeedLoader.kt

--- app/src/main/java/com/example/russianpath/data/seed/SeedLoader.kt
+++ app/src/main/java/com/example/russianpath/data/seed/SeedLoader.kt
@@ -3,33 +3,48 @@ package com.example.russianpath.data.seed
 import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class SeedLoader(
-    private val context: Context
+@Singleton
+class SeedLoader @Inject constructor(
+    @ApplicationContext
+    val context: Context
 ) {
 
-    private val gson = Gson()
+    val gson = Gson()
 
     inline fun <reified T> loadList(fileName: String): List<T> {
+
         return try {
+
             val json = context.assets
                 .open("seed/$fileName")
                 .bufferedReader()
                 .use { it.readText() }
 
             val type = object : TypeToken<List<T>>() {}.type
+
             gson.fromJson(json, type)
 
         } catch (e: Exception) {
+
             e.printStackTrace()
+
             emptyList()
+
         }
+
     }
 
     fun loadString(fileName: String): String {
+
         return context.assets
             .open("seed/$fileName")
             .bufferedReader()
             .use { it.readText() }
+
     }
+
 }
