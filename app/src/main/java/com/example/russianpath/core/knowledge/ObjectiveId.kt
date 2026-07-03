c6abc119cb2ada9d6cb12dcfecd5cada122885d6
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-03 19:59:18 +0300

    Update ObjectiveId.kt

--- app/src/main/java/com/example/russianpath/core/knowledge/ObjectiveId.kt
+++ app/src/main/java/com/example/russianpath/core/knowledge/ObjectiveId.kt
@@ -1,4 +1,19 @@
 package com.example.russianpath.core.knowledge
 
+/**
+ * Value Object для идентификатора цели обучения.
+ *
+ * Оборачивает строковый ID в типобезопасную обёртку.
+ * Предотвращает перепутывание ID разных сущностей.
+ */
 @JvmInline
-value class ObjectiveId(val value: String)
+value class ObjectiveId(val value: String) {
+
+    init {
+        require(value.isNotBlank()) {
+            "ObjectiveId cannot be blank"
+        }
+    }
+
+    override fun toString(): String = value
+}
